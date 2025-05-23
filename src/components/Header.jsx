143835f8c195f6752c656239011df5a4bd4be919
priyansh-0304 <arorapriyansh0304@gmail.com>
2025-05-23 22:10:54 +0530

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+const Header = () =>
+    {
+        const logo = "https://voicemaker.in/img/logo.svg";
+    
+        return(
+            <img src = {logo} alt = "logo" style = {{height:40 }}/>
+        )
+    }
+    
+    export default Header;
+    
